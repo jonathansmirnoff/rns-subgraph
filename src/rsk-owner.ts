de85f2791cb2f8205edd6908e3fc780762ecba15
Jonathan Smirnoff <jonathan.smirnoff@iovlabs.org>
2025-01-28 11:05:00 -0300

    feat: add RSKOwner contract to the subgraph

---
+++ src/rsk-owner.ts
@@ -0,0 +1,78 @@
+import { ByteArray, crypto, ens, log } from "@graphprotocol/graph-ts";
+import {
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  ExpirationChanged as ExpirationChangedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent,
+} from "../generated/RSKOwner/RSKOwner"
+import {  
+  Account,
+  Domain,  
+  NameRegistered,  
+  NameTransferred,  
+  Registration,  
+  Transfer,
+} from "../generated/schema"
+import { checkValidLabel, concat, createEventID, RSK_NODE, uint256ToByteArray } from "./utils";
+
+var rootNode: ByteArray = ByteArray.fromHexString(RSK_NODE);
+
+export function handleExpirationChanged(event: ExpirationChangedEvent): void {
+  let label = uint256ToByteArray(event.params.tokenId);  
+  let domain = Domain.load(crypto.keccak256(concat(rootNode, label)).toHex());
+  
+  // Workaround for the case when the domain is not found
+  if (domain == null) return ;
+
+  let registration = new Registration(label.toHex());
+
+  registration.domain = domain.id;
+  registration.registrationDate = event.block.timestamp;
+  registration.expiryDate = event.params.expirationTime;
+  registration.registrant = domain.owner;
+  
+  domain.expiryDate = event.params.expirationTime;
+
+  
+  let labelName = ens.nameByHash(label.toHexString());
+  if (checkValidLabel(labelName)) {
+    domain.labelName = labelName;
+    domain.name = labelName! + ".rsk";
+    registration.labelName = labelName;
+  }
+  domain.save();
+  registration.save();
+
+  let registrationEvent = new NameRegistered(createEventID(event));
+  registrationEvent.registration = registration.id;
+  registrationEvent.blockNumber = event.block.number.toI32();
+  registrationEvent.transactionID = event.transaction.hash;  
+  registrationEvent.expiryDate = event.params.expirationTime;
+  registrationEvent.registrant = domain.owner;
+  registrationEvent.save();
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let account = new Account(event.params.to.toHex());
+  account.save();
+
+  let label = uint256ToByteArray(event.params.tokenId);
+  let registration = Registration.load(label.toHex());
+  if (registration == null) return;
+
+  let domain = Domain.load(crypto.keccak256(concat(rootNode, label)).toHex())!;
+
+  registration.registrant = account.id;
+  domain.registrant = account.id;
+
+  domain.save();
+  registration.save();
+
+  let transferEvent = new NameTransferred(createEventID(event));
+  transferEvent.registration = label.toHex();
+  transferEvent.blockNumber = event.block.number.toI32();
+  transferEvent.transactionID = event.transaction.hash;
+  transferEvent.newOwner = account.id;
+  transferEvent.save();
+}
---
+++ tests/rsk-owner.test.ts
@@ -0,0 +1,31 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { Approval as ApprovalEvent } from "../generated/RSKOwner/RSKOwner"
+import { createApprovalEvent, createExpirationChangedEvent } from "./rsk-owner-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    //TODO: Implement test
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
+
+  test("Approval created and stored", () => {
+    //TODO: Implement test
+  })
+})
